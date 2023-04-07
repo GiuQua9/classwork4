96e739fb55797ca3aef03b7184a69cd760526dcd
GiuQua9 <giuseppe98.quaranta@gmail.com>
2023-04-07 22:42:59 +0200

    init server e client

--- src/service_client.cpp
+++ src/service_client.cpp
@@ -1,43 +1,31 @@
 #include "ros/ros.h"
-
-//Header of the service message. 
-//	The service message belongs to the ros_service package
-#include "ros_service/sum.h"
+#include "cwork4/tf_frame.h"
 
 using namespace std;
 
 int main(int argc, char **argv) {
 
 	//Init the ROS node with service_client name
-	ros::init(argc, argv, "service_client");
+	ros::init(argc, argv, "pose_client");
 	ros::NodeHandle n;
 
-	//Init the service client. Data to use for the service (the .srv file) and the name of the service
-	ros::ServiceClient client = n.serviceClient<ros_service::sum>("sum");
+	ros::ServiceClient client = n.serviceClient<cwork4::tf_frame>("pose");
 	
-	//Define and initialize the service data structure 
-	//	This datastructure brings with it the input value (in the request fields) and the output values, in the response field
-	ros_service::sum srv;
-	srv.request.x.data = 3.0;
-	srv.request.y.data = 10.0;
+	cwork4::tf_frame srv;
+	srv.request.frame_a = "/base_link";
+	srv.request.frame_b  = "/camera_link";
 	
-	//Wait that in the ROS network, the service sum is advertised
-	//	If you call a service and the service has not been advertised, you will have back an error
 	ROS_INFO("Waiting for the client server");
 	client.waitForExistence();
 	ROS_INFO("Client server up now");
 	
-	//Call the service callback
-	//	The return value is false if:
-	//		- the callback returns false
-	//		- the service has not been found in the ROS network
 	if (!client.call(srv)) {
 		ROS_ERROR("Error calling the service");
 		return 1;
 	}
 
 	//Just print the output
-	cout << "Service output: " << srv.response.sum << endl;
+	cout << "Service output: " << srv.response.pose << endl;
 	
 	return 0;
 }
--- src/service_server.cpp
+++ src/service_server.cpp
@@ -1,28 +1,20 @@
 #include "ros/ros.h"
-//Header of the service message. 
-//	The service message belongs to the ros_service package
-#include "ros_service/sum.h"
+#include "cwork4/tf_frame.h"
+#include <tf/transform_listener.h>
 
 using namespace std;
 
-//Callback function
-//	Return value: boolean
-//		If this function returns true, the service function has been corretly called
-//		You can use this value to check if the function has been called with correct parameters
-//		i.e. call a service calculating the square of a number, calling the service with a negative number
-//	Input values:  the request part of the servive 
-//				   the output of the service to fill
-bool service_callback( ros_service::sum::Request &req, ros_service::sum::Response &res) {
+bool service_callback( cwork4::tf_frame::Request &req, cwork4::tf_frame::Response &res) {
+	tf::TransformListener listener;
+	tf::StampedTransform transform;
+	try {
+		listener.lookupTransform(req.frame_a, req.frame_b, ros::Time(0), transform);
+	}
+	catch (tf::TransformException ex){
+		ROS_ERROR("%s",ex.what());
+		ros::Duration(1.0).sleep();
+	}
 
-
-	cout << "Service sum received: " << endl;
-	//We know that the service is called with 2 parameters: x and y
-	// These parameters are put in a data structure called req
-	cout << req.x << req.y << endl;
-
-	//The return value is store in the res datastrcutre
-	cout << "Returning the sum" << endl;
-	res.sum.data = req.x.data + req.y.data;
 
 	return true;
 }
@@ -30,12 +22,10 @@ bool service_callback( ros_service::sum::Request &req, ros_service::sum::Respons
 
 int main(int argc, char **argv) {
 
-	ros::init(argc, argv, "sum_service");
+	ros::init(argc, argv, "pose_service");
 	ros::NodeHandle n;
-
-	//Initialize the service object: name of the service and callback function
-	//	Like subscribers, also tje callback function can be declared as a class function
-	ros::ServiceServer service = n.advertiseService("sum", service_callback);
+	
+	ros::ServiceServer service = n.advertiseService("pose", service_callback);
 
 	//Call the spin function to maintain the node alive
 	ros::spin();
